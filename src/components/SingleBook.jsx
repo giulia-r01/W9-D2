207a2ef217beb603b884754be27e0d740e80e017
Giulia Rizzo <girzzo@gmail.com>
2025-04-02 16:24:28 +0200

    Update SingleBook.jsx

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,26 +1,38 @@
+import { Component } from "react"
 import { Container, Row, Col, Card, Button } from "react-bootstrap"
+import books from "../data-books/fantasy.json"
 
-const SingleBook = function (props) {
-  return (
-    <Container>
-      <Row className="justify-content-center g-3">
-        <Col
-          xs={12}
-          md={4}
-          lg={3}
-          key={props.asin}
-          className="justify-content-center d-flex align-items-stretch"
-        >
-          <Card className="d-flex flex-column w-100">
-            <Card.Img variant="top" src={props.img} />
-            <Card.Body className="d-flex flex-column testColor">
-              <Card.Title className="flex-grow-1">{props.title}</Card.Title>
-              <Button variant="dark">Compralo!</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  )
+class SingleBook extends Component {
+  state = {
+    bookState: {
+      selected: false,
+    },
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row className="justify-content-center g-3">
+          <Col
+            xs={12}
+            md={4}
+            lg={3}
+            key={this.props.asin}
+            className="justify-content-center d-flex align-items-stretch"
+          >
+            <Card className="d-flex flex-column w-100">
+              <Card.Img variant="top" src={this.props.img} />
+              <Card.Body className="d-flex flex-column testColor">
+                <Card.Title className="flex-grow-1">
+                  {this.props.title}
+                </Card.Title>
+                <Button variant="dark">Compralo!</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 export default SingleBook
